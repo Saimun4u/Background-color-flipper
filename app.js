86617da51df1221eba752417e17f427aae7b367a
Saimun <saimun4u@gmail.com>
2021-05-27 00:47:59 -0500

    background color change applied for simple color

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//DOM Selectors
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+    let randNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.background = colors[randNumber];
+    color.textContent = colors[randNumber];
+})
