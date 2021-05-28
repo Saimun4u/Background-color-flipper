1dae9a6aeb573b1ca75bec0852bcafc148e11828
Saimun <saimun4u@gmail.com>
2021-05-28 13:45:57 -0500

    Hex color js logic setup

--- hex.js
+++ hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+
+    let randNumber = Math.floor(Math.random() * hex.length);
+
+    for (let i = 0; i <= 5; i++){
+
+        let hexColor = 0;
+
+         hexColor[i] += `${hex[randNumber]}`
+
+         console.log(hexColor)
+
+    }
+    
+})
